79f4b2305daaa22e05de6ff27672ae0578f80132
nivethaaxess <nivetha.rajendran@axesstechnology.in>
2023-08-22 11:31:28 +0530

    Connection Create

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/BookingSlot', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// ... Rest of your code ...
+
+const port = 3005;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
